70362ad92b91620c4123b53a481a1ba3313d91e5
unknown <jordankudjoh06@gmail.com>
2026-04-19 16:35:48 +0000

    chore: optimize microsoft clarity script placement in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,18 +44,16 @@ export default function RootLayout({
             `,
           }}
         />
+      <body className={`${dmSans.variable} ${syne.variable} font-sans min-h-screen text-foreground antialiased`}>
         <Script id="microsoft-clarity" strategy="afterInteractive">
           {`
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "wdra4bg6ai");
-  `}
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "wdra4bg6ai");
+          `}
         </Script>
-
-      </head>
-      <body className={`${dmSans.variable} ${syne.variable} font-sans min-h-screen text-foreground antialiased`}>
         <Toaster position="top-center" richColors expand={true} />
         <ThemeInitializer />
         <PWARegister />
